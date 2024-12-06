53dd21c6731316e99941e53483fa1d428837bcb9
Sandhya Nayak <107762980+sandhya2505@users.noreply.github.com>
2024-12-06 12:14:43 +0530

    Add files via upload
    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { NgClass, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  todoList: TodoItem[] = [];
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false,
+      };
+
+      this.todoList.push(newTodoItem);
+      this.newTask = ' ';
+    }
+  }
+  toggleCompleted(index: number): void {
+    console.log(index);
+    this.todoList[index].completed = !this.todoList[index].completed;
+  }
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter((item) => item.id !== id);
+    console.log(this.todoList);
+  }
+}
